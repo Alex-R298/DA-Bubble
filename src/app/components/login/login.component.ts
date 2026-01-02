bc7b164e67b3932ab3e694b78885663052bd1ccf
ttariik <tarik.sabanovic2@icloud.com>
2026-01-02 19:19:09 +0100

    (DES): Integrated SVG-Images component with Google icon and updated login component to use it

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { SvgImagesComponent } from '../svg-images/svg-images.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, SvgImagesComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/svg-images/svg-images.component.ts
+++ src/app/svg-images/svg-images.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-svg-images',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './svg-images.component.html',
   styleUrl: './svg-images.component.css'
 })
 export class SvgImagesComponent {
+  @Input() iconName: string = '';
+  @Input() width: string = '38';
+  @Input() height: string = '38';
+  @Input() className: string = '';
 
+  isGoogleIcon(): boolean {
+    return this.iconName === 'google';
+  }
 }
