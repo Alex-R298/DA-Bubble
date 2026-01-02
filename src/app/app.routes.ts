25ca0d208570855a88d36d6ab75412e3b928316d
ttariik <tarik.sabanovic2@icloud.com>
2026-01-02 15:34:56 +0100

    (DES): Add guest login button to login component with navigation to dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { ImprintComponent } from './components/imprint/imprint.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -10,6 +11,7 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'intro', component: IntroComponent },
   { path: 'imprint', component: ImprintComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '**', redirectTo: '' }
 ];
 
