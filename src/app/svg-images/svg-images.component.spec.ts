c257e35e8f6d2c00d38b0708a025109295974b7f
ttariik <tarik.sabanovic2@icloud.com>
2026-01-02 19:03:40 +0100

    (DES): Created svg-images component

---
+++ src/app/svg-images/svg-images.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SvgImagesComponent } from './svg-images.component';
+
+describe('SvgImagesComponent', () => {
+  let component: SvgImagesComponent;
+  let fixture: ComponentFixture<SvgImagesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SvgImagesComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SvgImagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/svg-images/svg-images.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-images',
+  standalone: true,
+  imports: [],
+  templateUrl: './svg-images.component.html',
+  styleUrl: './svg-images.component.css'
+})
+export class SvgImagesComponent {
+
+}
