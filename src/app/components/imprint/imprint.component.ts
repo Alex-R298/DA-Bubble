30c4247b02b75380dfa1af87707b22c8c00ad095
ttariik <tarik.sabanovic2@icloud.com>
2026-01-04 15:02:29 +0100

    (DES): Redesigned imprint component to match privacy policy design

--- src/app/components/imprint/imprint.component.ts
+++ src/app/components/imprint/imprint.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SvgImagesComponent } from '../../svg-images/svg-images.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterModule, SvgImagesComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.css'
 })
 export class ImprintComponent {
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
 }
 
